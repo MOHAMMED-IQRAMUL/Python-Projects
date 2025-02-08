aad14e053523aa386a94a6a24ca42706be7f3bf1
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-02-08 19:24:49 +0530

    Add web scraping methods and update documentation

---
+++ WebScraping/Method_1/RequestHTML.py
@@ -0,0 +1,10 @@
+from requests_html import HTMLSession
+
+session = HTMLSession()
+
+r = session.get('https://python.org/')
+
+print(r.html.links)
+
+about = r.html.find('#about', first=True)
+print(about.text)
